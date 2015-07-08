73a9c4d7b43e8d297bc9474d22a97fe65c23a8f6
Kamil Sikora <kamil.sikora1@ingbank.pl>
2015-07-08 12:16:19 +0200

    Backup scheduling

--- src/rotator/__init__.py
+++ src/rotator/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from rotator.schedule import scheduler
 from rotatordb import db_session
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app.config.from_object('app_config')
 from rotator import db_config
 
 app.config.update(db_config.load_config())
+scheduler.start()
 
 from rotator import model
 
---
+++ src/rotator/schedule/__init__.py
@@ -0,0 +1,9 @@
+import app_config
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+from apscheduler.schedulers.background import BackgroundScheduler
+from rotator.schedule.listeners import execution_listener, error_listener
+
+scheduler = BackgroundScheduler()
+scheduler.add_jobstore('sqlalchemy', url=app_config.DATABASE_URI)
+scheduler.add_listener(execution_listener, EVENT_JOB_EXECUTED)
+scheduler.add_listener(error_listener, EVENT_JOB_ERROR)
