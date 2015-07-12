dd31b3ab1e6b594073b73a3af5cff9700983e0ff
johnnyFiftyFive <sikorekk@gmail.com>
2015-07-12 22:48:18 +0200

    The oldest file removal task (cherry picked from commit 85377ae)

--- src/db_back.py
+++ src/db_back.py
@@ -3,10 +3,12 @@ import datetime
 import subprocess
 
 import os
+from os.path import getmtime
 import mysql.connector
-from rotator import app, db_log
+from rotator import db_log
 from rotator.keys import BACK_DB_USER, BACK_DB_PASS, BACK_DB_HOST, BACK_DB_NAME
 from rotatordb import Backup, db_session
+from starter import app
 
 
 def get_connection():
@@ -64,3 +66,35 @@ def create_backup():
     else:
         db_log.error(u'Błąd przy wykonywaniu zrzutu.', dict(command=command, errCode=result))
     return result
+
+
+def delete(back_id):
+    back = db_session.query(Backup).filter(Backup.id == back_id).first()
+    back.status = 255
+    resp = dict(status='ok')
+    try:
+        os.remove('backups/{}'.format(back.filename))
+        db_log.info('Usunieto plik kopii', dict(file=back.filename))
+    except Exception as err:
+        resp['status'] = 'error'
+        resp['filename'] = back.filename
+        db_log.error('Nie mozna usunac pliku', dict(file=back.filename, error=err.message))
+    if resp['status'] == 'ok':
+        db_session.merge(back)
+        db_session.commit()
+    return resp
+
+
+def remove_oldest(directory, count):
+    """
+    Deletes the oldest files from given directory
+
+    :param directory: directory to prune
+    :param count: number of files, which will survive
+    """
+    files = os.listdir(directory)
+    if len(files) < count:
+        return
+    files.sort(key=lambda f: getmtime(f))
+    for f in files[:count]:
+        os.remove(f)
--- src/scratchrotator.py
+++ src/scratchrotator.py
@@ -1,31 +1,6 @@
 # coding=utf-8
-import os
-from os.path import getmtime
-from rotator import app
-from rotator.keys import BACK_DB_CONFIG_KEYS
+from starter import app
 
 if __name__ == '__main__':
     app.run()
 
-
-def check_db_config():
-    lacking = []
-    for k in BACK_DB_CONFIG_KEYS:
-        if not app.config.get(k):
-            lacking.append(k)
-    return lacking
-
-
-def remove_oldest(directory, count):
-    """
-    Deletes the oldest files from given directory
-
-    :param directory: directory to prune
-    :param count: number of files, which will survive
-    """
-    files = os.listdir(directory)
-    if len(files) < count:
-        return
-    files.sort(key=lambda f: getmtime(f))
-    for f in files[:count]:
-        os.remove(f)
