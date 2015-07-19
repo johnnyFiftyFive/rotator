63d3e1b0e2e0de8d15a8827c9333eb83244e2594
johnnyFiftyFive <sikorekk@gmail.com>
2015-07-19 15:55:27 +0200

    Refactor

--- src/starter.py
+++ src/starter.py
@@ -1,7 +1,10 @@
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 
 from rotator.keys import SCHED_HOUR, SCHED_MINUTE, SCHED_JOBID
-from rotator.schedule import scheduler
+from rotator.schedule.listeners import execution_listener
+from rotator.schedule.listeners import error_listener
 
 app = Flask(__name__)
 app.config.from_object('app_config')
@@ -12,6 +15,9 @@ app.config.update(db_config.load_config())
 
 import db_back
 
+scheduler = BackgroundScheduler()
+scheduler.add_listener(execution_listener, EVENT_JOB_EXECUTED)
+scheduler.add_listener(error_listener, EVENT_JOB_ERROR)
 scheduler.add_job(db_back.create_backup, 'cron',
                   hour=int(app.config[SCHED_HOUR]),
                   minute=int(app.config[SCHED_MINUTE]),
